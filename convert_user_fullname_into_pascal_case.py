3ee4a55d8414c85afdea28449391c5b064a04357
FudgeeBarr03 <fudgeebar024@gmail.com>
2025-05-08 17:20:46 +0800

    print user input in pascal case

--- convert_user_fullname_into_pascal_case.py
+++ convert_user_fullname_into_pascal_case.py
@@ -1,3 +1,5 @@
 #Ask for user full name not in pascal case
 fullname = input("enter your full name not in pascal case: ")
+
 #convert user input into correct pascal case
+print(''.join(word.capitalize() for word in fullname.split()))
