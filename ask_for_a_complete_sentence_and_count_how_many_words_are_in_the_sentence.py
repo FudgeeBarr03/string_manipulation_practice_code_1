e0b12582260aa53bbfb92da36751aeecd297d7de
FudgeeBarr03 <fudgeebar024@gmail.com>
2025-05-08 16:59:57 +0800

    output how many words are in the sentence

--- ask_for_a_complete_sentence_and_count_how_many_words_are_in_the_sentence.py
+++ ask_for_a_complete_sentence_and_count_how_many_words_are_in_the_sentence.py
@@ -1,3 +1,5 @@
 #Ask for user to input a full sentence
 sentence = input("Enter a full statement: ")
+
 #count the words and print how many are in the sentence
+print(f"there are {len(sentence.split())} words in the sentence")
